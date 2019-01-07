d39bf0a865a0f533feb06c881c53a2fb6174218b
anujsinghwd <anuj_singh@outlook.in>
2019-01-08 01:25:38 +0530

    fs

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ app.use('/api/posts', posts);
 // Server static assetsif in production
 if(process.env.NODE_ENV !== 'production'){
     // Set a satic folder
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
-    app.get('/*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
