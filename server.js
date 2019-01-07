936615ea1be53d64ca3f0b1da23ef3d0bea7498a
anujsinghwd <anuj_singh@outlook.in>
2019-01-08 01:09:58 +0530

    make it better

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // Server static assetsif in production
-if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV !== 'production'){
     // Set a satic folder
     app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
